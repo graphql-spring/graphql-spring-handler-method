e2d257404b5d86665b05f60d923a3e0db7034ea0
Yunchan-Oh <oyunchan9@gmail.com>
2025-04-01 23:56:03 +0900

    init

---
+++ src/test/java/com/graphql/handler/HandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graphql.handler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandlerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
